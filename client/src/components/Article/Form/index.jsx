c4f5c57aaed5072e3250368ce8da59cc83214279
Chris Haack <chris.haack@me.com>
2020-04-04 10:45:03 -0400

    refactor form with useState hooks

--- client/src/components/Article/Form/index.jsx
+++ client/src/components/Article/Form/index.jsx
@@ -1,24 +1,15 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from "react";
 import { connect } from 'react-redux';
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
+function Form(props) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
 
-    this.state = {
-      title: '',
-      body: '',
-      author: '',
-    }
 
-    this.handleChangeField = this.handleChangeField.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(){
-    const { onSubmit } = this.props;
-    const { title, body, author } = this.state;
+  const handleSubmit = (evt) => {
+    const { onSubmit } = props;
 
     return axios.post('http://localhost:8000/api/articles', {
       title,
@@ -28,39 +19,29 @@ class Form extends React.Component {
       .then((res) => onSubmit(res.data));
   }
 
-  handleChangeField(key, event) {
-    this.setState({
-      [key]: event.target.value,
-    });
-  }
-
-  render() {
-    const { title, body, author } = this.state;
-
-    return (
-      <div className="col-12 col-lg-6 offset-lg-3">
-        <input
-          onChange={(ev) => this.handleChangeField('title', ev)}
-          value={title}
-          className="form-control my-3"
-          placeholder="Article Title"
-        />
-        <textarea
-          onChange={(ev) => this.handleChangeField('body', ev)}
-          className="form-control my-3"
-          placeholder="Article Body"
-          value={body}>
-        </textarea>
-        <input
-          onChange={(ev) => this.handleChangeField('author', ev)}
-          value={author}
-          className="form-control my-3"
-          placeholder="Article Author"
-        />
-        <button onClick={this.handleSubmit} className="btn btn-primary float-right">Submit</button>
-      </div>
-    )
-  }
+  return (
+    <div className="col-12 col-lg-6 offset-lg-3">
+      <input
+        onChange={e => setTitle(e.target.value)}
+        value={title}
+        className="form-control my-3"
+        placeholder="Article Title"
+      />
+      <textarea
+        onChange={e => setBody(e.target.value)}
+        className="form-control my-3"
+        placeholder="Article Body"
+        value={body}>
+      </textarea>
+      <input
+        onChange={e => setAuthor(e.target.value)}
+        value={author}
+        className="form-control my-3"
+        placeholder="Article Author"
+      />
+    <button onClick={handleSubmit} className="btn btn-primary float-right">Submit</button>
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
